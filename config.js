6caae1bc15712ef64b08fe98c0f8065a910ee442
rdtyzbotz <113617871+rdtyzbotz@users.noreply.github.com>
2022-10-24 16:23:26 +0700

    Update config.js

--- config.js
+++ config.js
@@ -92,7 +92,7 @@ global.hbunny = ['https://telegra.ph/file/2b71a8d46d29351479fbc.jpg', 'https://t
 global.hbeach = ['https://telegra.ph/file/14ae0ba2da77d74e6b80c.jpg', 'https://telegra.ph/file/b6905b77e6c7732592a13.jpg','https://telegra.ph/file/9da45a352eb4c40e5d641.jpg', 'https://telegra.ph/file/59e78846ee365975ee6e3.jpg','https://telegra.ph/file/1bf7dee46b83eb4c41d7d.jpg','https://telegra.ph/file/0525a7929f819cb8278f3.jpg']
 global.bg = ['https://telegra.ph/file/2b71a8d46d29351479fbc.jpg', 'https://telegra.ph/file/ae610571b62b5ab876e9c.jpg', 'https://telegra.ph/file/cc8255d5b989eef587af2.jpg','https://telegra.ph/file/30d2e7375996abd9cfee3.jpg']
 /*============== NOMOR ==============*/
-global.nomorbot = '6281268416245'
+global.nomorbot = '62823347039872'
 global.nomorown = '17608914335'
 global.namebot = 'Sad-Bot'
 global.nameown = 'Papah-Chan'
